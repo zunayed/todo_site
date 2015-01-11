b9603c401f4f4531bd8f0c89cfe9bea583308e86
Zunayed Ali <zunayed@gmail.com>
2015-01-11 16:02:24 -0500

    add urls for endpoints

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, url
 
+from todo_app import views
+
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^todos/$', views.todos, name='todos'),
+    url(r'^todos/(?P<id>.+)$', views.get_todo, name='get_todo'),
+
 )
