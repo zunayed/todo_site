287fa4cd866d88ce21b18ba39b9a8fe38f255081
Zunayed Ali <zunayed@gmail.com>
2015-01-11 15:58:04 -0500

    test stubs

---
+++ todo_app/tests.py
@@ -0,0 +1,29 @@
+
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_todo_models(self):
+        pass
+
+
+class ViewTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_all_todos(self):
+        pass
+
+    def test_get_one_todo(self):
+        pass
+
+    def test_update_todo(self):
+        pass
+
+    def test_delete_todo(self):
+        pass
